89760b499edd7106c96362a76fceddb383d7bddd
Elisei TATARU <elisei.tataru@gmail.com>
2024-09-04 17:45:07 +0300

    Add Readme, and sample Input and Output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,15 @@ package ro.etataru.storemgmt
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+    val input = arrayOf("aab", "ac", "dada", "d", "ba", "caa", "baba", "ddd")
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    print("Input: ")
+    for (x in input) print("$x, ")
+    println()
+    println("expected Output: ")
+    println("aab  -> 3   // [aab, ba, baba]")
+    println("ac   -> 2   // [ac, caa]")
+    println("dada -> 1   // [dada]")
+    println("d    -> 1   // [d, ddd]")
 }
